8851ef9b7ee1709a55f63fde06434154154914c6
Vampire <rupalidhakate72@gmail.com>
2025-12-02 16:01:24 +0530

    ArithmeticOperationsBasics

---
+++ Arithmetic-Operations_Basic/src/main/kotlin/Main.kt
@@ -0,0 +1,49 @@
+//region Addition
+fun addition(){
+    val x=5
+    val y=10
+    //We have taken 2 variable x and y with value 5 and 10 respectively. You can change values and names!
+    println(x+y)
+}
+//endregion
+
+//region Subtraction
+fun subtraction(){
+    val x=10
+    val y=7
+    //We have taken 2 variable x and y with value 10 and 7 respectively. You can change values and names!
+    println(x-y)
+}
+//endregion
+
+//region Multiplication
+fun multiplication(){
+    val x=10
+    val y=7
+    //We have taken 2 variable x and y with value 10 and 7 respectively. You can change values and names!
+    println(x*y)
+}
+//endregion
+
+//region Division
+fun division(){
+    val x=10
+    val y=2
+    //We have taken 2 variable x and y with value 10 and 2 respectively. You can change values and names!
+    println(x/y)
+}
+//endregion
+
+fun main(){
+    println("Doing Addition...")
+    addition()
+
+    println("Doing Subtraction...")
+    subtraction()
+
+    println("Doing Multiplication...")
+    multiplication()
+
+    println("Doing Division...")
+    division()
+}
